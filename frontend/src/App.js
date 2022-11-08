41b5b7eb1307edf8b216597f7ff99286f9939c8b
4R115 <mojtaba.samani@outlook.com>
2022-11-08 19:23:24 +0330

    we created a product component than created a rating component than in the App.js we use product component and we passed properties or props to product component.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,7 @@ function App() {
             <div className="row center">
               {
                 data.products.map(product => (
-                  <Product key={product._id} product={product}></product>
+                  <Product key={product._id} product={product}></Product>
                 ))
               }
             </div>
